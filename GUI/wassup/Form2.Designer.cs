e06a40cb48fe76346bcf186b9f7035fa6fae0537
Parth Saravade <73926727+PsychedelicOrange@users.noreply.github.com>
2022-08-22 18:42:08 +0530

    Added vc# gui

---
+++ GUI/wassup/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace wassup
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.msgInput = new System.Windows.Forms.RichTextBox();
+            this.chat = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.msgInput);
+            this.splitContainer1.Panel2.Controls.Add(this.chat);
+            this.splitContainer1.Size = new System.Drawing.Size(924, 458);
+            this.splitContainer1.SplitterDistance = 288;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(288, 458);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Chat";
+            this.columnHeader1.Width = 282;
+            // 
+            // msgInput
+            // 
+            this.msgInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.msgInput.Location = new System.Drawing.Point(0, 421);
+            this.msgInput.Name = "msgInput";
+            this.msgInput.Size = new System.Drawing.Size(632, 37);
+            this.msgInput.TabIndex = 1;
+            this.msgInput.Text = "";
+            this.msgInput.TextChanged += new System.EventHandler(this.msgInput_TextChanged);
+            this.msgInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.msgInput_KeyDown);
+            // 
+            // chat
+            // 
+            this.chat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.chat.Location = new System.Drawing.Point(0, 0);
+            this.chat.Name = "chat";
+            this.chat.Size = new System.Drawing.Size(632, 421);
+            this.chat.TabIndex = 0;
+            this.chat.Text = "";
+            this.chat.WordWrap = false;
+            this.chat.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 458);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.RichTextBox msgInput;
+        private System.Windows.Forms.RichTextBox chat;
+        private System.Windows.Forms.Timer timer1;
+        
+        
+    }
+}
---
+++ GUI/wassup/dHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.DataAccess.Client;
+namespace wassup
+{
+    public static class dHelper
+    {
+        public static DataSet GetDataSet(string command)
+        {
+            try
+            {
+                string ConString = "USER ID='\"C##PARTH\"';PASSWORD=oracle;DATA SOURCE=127.0.0.1:1521/ORCL;PERSIST SECURITY INFO=True";
+                using (OracleConnection con = new OracleConnection(ConString))
+                {
+                    OracleCommand cmd = new OracleCommand(command, con);
+                    OracleDataAdapter oda = new OracleDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    oda.Fill(ds);
+                    return ds;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(" : SQL ERROR !");
+                return null;
+            }
+        }
+        public static void SetDataSet()
+        {
+
+        }
+        public static Object SendMessage(string command)
+        {
+            try
+            {
+                string ConString = "USER ID='\"C##PARTH\"';PASSWORD=oracle;DATA SOURCE=127.0.0.1:1521/ORCL;PERSIST SECURITY INFO=True";
+                using (OracleConnection con = new OracleConnection(ConString))
+                {
+                    con.Open();
+                    OracleCommand cmd = new OracleCommand(command, con);
+                    return cmd.ExecuteReader();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message + " : SQL ERROR !");
+                return null;
+            }
+        }
+        public static Object ExecuteCommand(string command)
+        {
+            try
+            {
+                string ConString = "USER ID='C##PARTH';PASSWORD=oracle;DATA SOURCE=127.0.0.1:1521/ORCL;PERSIST SECURITY INFO=True";
+                using (OracleConnection con = new OracleConnection(ConString))
+                {
+                    con.Open();
+                    OracleCommand cmd = new OracleCommand(command, con);
+                    return cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message+" : SQL ERROR !");
+                return null;
+            }
+        }
+    }
+}
